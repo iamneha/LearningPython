91d19bdf83cba635d2d4f66b4b940d3edf566286
Neha Yadav <inehayadav28@gmail.com>
2016-02-16 23:49:25 +0530

    More variables and Printing

---
+++ ex5.py
@@ -0,0 +1,15 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'white'
+hair = 'Brown'
+
+print "Let's talk about %r." % name
+print "He's %r inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy"
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+print "If I add %d, %d, and  %d I get %d." % (age, height, weight, age + height + weight) 
